337608c280614137d3a96aa6f8611a2452995926
Daniel Escalante <d.escalante@uniandes.edu.co>
2024-04-02 09:00:50 -0500

    Reto completado

---
+++ src/components/__tests__/like.test.js
@@ -0,0 +1,24 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import Like from "../like"
+
+test("Check Likes: 0 by default", () => {
+    render(<Like></Like>);
+    const p = screen.getByTestId("likes");
+    expect(p.innerHTML).toBe("Likes: 0");
+});
+
+test("Check increment in like click", () => {
+    render(<Like></Like>);
+    const increment = screen.getByTestId("increment");
+    const p = screen.getByTestId("likes");
+    fireEvent.click(increment)
+    expect(p.innerHTML).toBe("Likes: 1");
+});
+
+test("Check decrement in like click", () => {
+    render(<Like></Like>);
+    const decrement = screen.getByTestId("decrement");
+    const p = screen.getByTestId("likes");
+    fireEvent.click(decrement)
+    expect(p.innerHTML).toBe("Likes: -1");
+});
---
+++ src/components/like.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+class Like extends Component {
+  state = {
+    likes: 0,
+  };
+
+  handleIncrement = () => {
+    this.setState({ likes: this.state.likes + 1 });
+  };
+
+  handleDecrement = () => {
+    this.setState({ likes: this.state.likes - 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p data-testid="likes">Likes: {this.state.likes}</p>
+        <button data-testid="increment" id="increment" onClick={this.handleIncrement}>
+          Like
+        </button>
+        <button data-testid="decrement" id="decrement" onClick={this.handleDecrement}>
+          {" "}
+          Dislike
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Like;
